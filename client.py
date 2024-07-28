c9db964f4a527376b5a76db35d136f8d1957237a
Cafeko <caiofgs2329@gmail.com>
2024-07-27 22:26:18 -0300

    Update: QTable Class - setQTableValue function

--- client.py
+++ client.py
@@ -1,3 +1,5 @@
+# Feito por: Caio Ferreira Gomes da Silva
+
 import connection as cn
 import random
 
@@ -9,33 +11,33 @@ class Qtable:
     def __init__(self):
         self._start_value = 0.000000
         self._table_size = 96
-        self._table = {"giro_esq": [], "giro_dir": [], "pulo": []}
+        self._table = {"left": [], "right": [], "jump": []}
 
     # Cria uma tabela com o tamanho definido e um valor padrão pata todos os elementos.
     def resetQTable(self):
-        self._table = {"giro_esq": ([self._start_value] * self._table_size), "giro_dir": ([self._start_value] * self._table_size), "pulo": ([self._start_value] * self._table_size)}
+        self._table = {"left": ([self._start_value] * self._table_size), "right": ([self._start_value] * self._table_size), "jump": ([self._start_value] * self._table_size)}
     
     # Gera a tabela Q com valores aleatorios.
     def randomizeQTable(self):
-        self._table = {"giro_esq": [], "giro_dir": [], "pulo": []} # Limpa a tabela.
+        self._table = {"left": [], "right": [], "jump": []} # Limpa a tabela.
         for i in range(self._table_size):
             # Gera os valores aleatorios e os adiciona a tabela.
             value = Qtable._generateRandomValue()
-            self._table["giro_esq"].append(value)
+            self._table["left"].append(value)
             value = Qtable._generateRandomValue()
-            self._table["giro_dir"].append(value)
+            self._table["right"].append(value)
             value = Qtable._generateRandomValue()
-            self._table["pulo"].append(value)
+            self._table["jump"].append(value)
 
-    # Gera um float aleatorio entre 0 e 10, com precisão de 6 casas decimais.
+    # Gera um float aleatorio entre -10 e 10, com precisão de 6 casas decimais.
     def _generateRandomValue():
-        return round(random.uniform(0.0, 10.0), 6)
+        return round(random.uniform(-10.0, 10.0), 6)
 
     # Salva o conteudo da tabelaQ da classe para o arquivo.
     def save(self, path):
         with open(path, "w") as file:
             for i in range(self._table_size):
-                line = f"{self._table["giro_esq"][i]:.6f} {self._table["giro_dir"][i]:.6f} {self._table["pulo"][i]:.6f}"
+                line = f"{self._table["left"][i]:.6f} {self._table["right"][i]:.6f} {self._table["jump"][i]:.6f}"
                 file.write(line + "\n")
             file.close()
     
@@ -46,10 +48,24 @@ class Qtable:
             for line in file_lines:
                 values = line.rstrip("\n").split(" ") # rstrip remove o \n no fim da linha; split separa os valores da linha.
                 # Salva os valores em suas respectivas colunas:
-                self._table["giro_esq"].append(float(values[0]))
-                self._table["giro_dir"].append(float(values[1]))
-                self._table["pulo"].append(float(values[2]))
+                self._table["left"].append(float(values[0]))
+                self._table["right"].append(float(values[1]))
+                self._table["jump"].append(float(values[2]))
             file.close()
+    
+    # Muda um valor na tabela Q para um estado e ação especifico.
+    def setQTableValue(self, state: int, action: str, value: float):
+        if Qtable._isValidAction(action):
+            if state < self._table_size:
+                self._table[action][state] = value
+            else:
+                print("ERRO: ESTADO INVALIDO")
+        else:
+            print("ERRO: AÇÃO INVALIDA")
+    
+    # Verifica se a ação recebida é uma ação valida.
+    def _isValidAction(action: str):
+        return action == "left" or action == "right" or action == "jump"
 
 
 # --- CONVERTER:
@@ -73,9 +89,9 @@ def reset():
 def learn():
     q_table = Qtable()
     q_table.load(q_table_path)
-    s = cn.connect(2037)
-    state, value = cn.get_state_reward(s, "jump")
-    print(f"State: {binaryInt(state)}")
+    """s = cn.connect(2037)
+    state, value = cn.get_state_reward(s, "jump")"""
+    q_table.setQTableValue(0, "jump", 10)
     q_table.save(q_table_path)
 
 # Recria a tabela Q com valores aleatorios:
