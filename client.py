bc660c07ffe0ce282f92da608e6a1aad9c74ada3
Cafeko <caiofgs2329@gmail.com>
2024-07-30 15:24:54 -0300

    Add: QLearning algorithm

--- client.py
+++ client.py
@@ -87,6 +87,20 @@ class Qtable:
         else:
             print("ERRO: ESTADO INVALIDO")
 
+    def getNextAction(self, state: int):
+        if state < self._table_size:
+            action_left = self._table["left"][state]
+            action_right = self._table["right"][state]
+            action_jump = self._table["jump"][state]
+            if action_left > action_right and action_left > action_jump:
+                return "left"
+            elif action_right > action_jump:
+                return "right"
+            else:
+                return "jump"
+        else:
+            print("ERRO: ESTADO INVALIDO") 
+
 
 # --- CONVERTER:
 # Trnasforma uma string de binario para um numero inteiro.
@@ -96,6 +110,50 @@ def binaryInt(binary: str):
 # Transforma um numero inteiro em uma string de binario.
 def intBinary(integer: int):
     return bin(integer)
+
+
+
+# --- Q Learning:
+def qLearning(q_table: Qtable, start_state: int, random_action: float, action_number=1000, gamma=0.7, alpha=0.5):
+    """
+    q_table: objeto da classe QTable que contem a tabela que será usada para o aprendizado.
+    start_state: estado que está no inicio do treinamento.
+    action_number: numero de ações que irá fazer, treinamento acaba ao atingir dado umero de ações.
+    gamma: valor de desconto, determina o quanto que a recompenssa futura e valorizada em relação a recompensa atual.
+    alpha: taxa de aprendizado.
+    """
+    # Preparação:
+    s = cn.connect(2037)
+    current_state = start_state
+    act = ""
+    for a in range(action_number):
+        print(a+1)
+        # escolhe ação:
+        if random.random() < random_action:
+            act = random.choice(["left", "right", "jump"])
+        else:
+            act = q_table.getNextAction(current_state)
+        # executa ação:
+        state_binary, reward = cn.get_state_reward(s, act)
+        new_state = binaryInt(state_binary)
+        # atualiza valor da ação na tabela:
+        current_state_action_value = q_table.getValue(current_state, act) # Pega o valor da ação feita, no estado atual.
+        q_estimate = bellmanEquation(q_table, new_state, reward, gamma)   # Usa a equação de Bellman para obter a estimativa de Q(s,a).
+        error = q_estimate - current_state_action_value                   # Obtem o erro fazendo a estmativa de Q(s,a) menos o valor relacionado a ação e o estado atual.
+        update_value = current_state_action_value + (alpha * error)       # Obtem o novo valor da ação, fazendo a soma do valor atual mais o error vezes a taxa de aprendizado.
+        q_table.setValue(current_state, act, update_value)                # Atualiza a tabela com o valor obtido.
+        # atualiza estado:
+        current_state = new_state
+
+def bellmanEquation(q_table: Qtable, new_state: int, reward, gamma: float):
+    """
+    q_table: objeto da classe QTable que contem a tabela que será usada para o aprendizado.
+    new_state: Estado em que foi parar após a ter feito a ação.
+    reward: recompensa do novo estado.
+    gamma: valor de desconto, determina o quanto que a recompenssa futura e valorizada em relação a recompensa atual.
+    """
+    return reward + (gamma * q_table.getMaxValue(new_state))
+
 
 
 # --- EXECUÇÃO:
@@ -109,11 +167,12 @@ def reset():
 def learn():
     q_table = Qtable()
     q_table.load(q_table_path)
-    """s = cn.connect(2037)
-    state, value = cn.get_state_reward(s, "jump")"""
-    q_table.setValue(0, "jump", 10)
-    print(q_table.getValue(2, "right"))
-    print(q_table.getMaxValue(5))
+    start_state = 0
+    random_act = 0.5
+    n_actions = 5000
+    gamma = 0.85
+    alpha = 0.3
+    qLearning(q_table, start_state, random_act, n_actions, gamma, alpha)
     q_table.save(q_table_path)
 
 # Recria a tabela Q com valores aleatorios:
